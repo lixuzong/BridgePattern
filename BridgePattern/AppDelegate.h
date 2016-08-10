195d13db8fc9ac89511fd8733d30697edc1769a4
lixuzong <lixuzong@163.com>
2016-08-10 09:47:22 +0800

    Initial Commit

---
+++ BridgePattern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgePattern
+//
+//  Created by Richard on 16/8/10.
+//  Copyright © 2016年 Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BridgePattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BridgePattern
+//
+//  Created by Richard on 16/8/10.
+//  Copyright © 2016年 Richard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
